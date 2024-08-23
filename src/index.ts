386dc252efa0d21b16bbf386a113b2c57b7ac09f
yamasaw <tonigthisyamada@yamasaw.com>
2024-08-23 18:06:35 +0900

    SupabaseのAuth機能を利用

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
+import { ClientErrorStatusCode } from 'hono/utils/http-status'
 import { createClient } from '@supabase/supabase-js'
-
 
 
 type Bindings = {
@@ -15,7 +15,55 @@ app.get('/', async (c) => {
   return c.json({ message: 'Hello World' })
 })
 
+app.post('/login/', async(c) => {
+  const body = await c.req.json()
+  const supabaseUrl = c.env.SUPABASE_URL
+  const supabaseKey = c.env.SUPABASE_KEY
+  const supabase = createClient(supabaseUrl, supabaseKey)
+  const { data, error } =  await supabase.auth.signInWithPassword(body)
+  if (error) {
+    return c.json({ message: error.message }, error.status as ClientErrorStatusCode)
+  }
+  return c.json(data)
+})
+
+app.post('/logout/', async(c) => {
+  const supabaseUrl = c.env.SUPABASE_URL
+  const supabaseKey = c.env.SUPABASE_KEY
+  const supabase = createClient(supabaseUrl, supabaseKey)
+  const { error } =  await supabase.auth.signOut()
+  if (error) {
+    return c.json({ message: error.message }, error.status as ClientErrorStatusCode)
+  }
+  return c.json({ message: 'Logged out' })
+})
+
+app.post('/signup/', async(c) => {
+  const body = await c.req.json()
+  const supabaseUrl = c.env.SUPABASE_URL
+  const supabaseKey = c.env.SUPABASE_KEY
+  const supabase = createClient(supabaseUrl, supabaseKey)
+  const { data, error } = await supabase.auth.signUp(body)
+
+  if (error) {
+    return c.json({ message: error.message }, error.status as ClientErrorStatusCode)
+  }
+
+  return c.json(data)
+})
+
+// TODO 認証ユーザの一覧を取得
 app.get('/users/', async (c) => {
+  const supabaseUrl = c.env.SUPABASE_URL
+  const supabaseKey = c.env.SUPABASE_KEY
+  const supabase = createClient(supabaseUrl, supabaseKey)
+  const { data, error } = await supabase.from('users').select('*')
+
+  return c.json(data)
+})
+
+// TODO 認証ユーザの詳細情報を取得
+app.get('/users/:id', async (c) => {
   const supabaseUrl = c.env.SUPABASE_URL
   const supabaseKey = c.env.SUPABASE_KEY
   const supabase = createClient(supabaseUrl, supabaseKey)
